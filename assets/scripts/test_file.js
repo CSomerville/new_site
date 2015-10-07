4adbe0de977204b66e7c0ad7d704d12d071ff999
Colby Somerville <colby.somerville@gmail.com>
2015-10-07 11:54:06 -0400

    hello falling skills

--- assets/scripts/test_file.js
+++ assets/scripts/test_file.js
@@ -1,1 +1,14 @@
-var unk;
+var allSkills = ['git', 'nodejs', 'postgresql', 'backbone', 'less', 
+                'sass', 'gulp', 'angular', 'ruby', 'rails', 'python']
+
+function makeMeteors(){
+  var skill = allSkills[Math.floor(Math.random () * allSkills.length)];
+  var str = '<span class="devicons devicons-' + skill + '">';
+  var $el = $(str).addClass('heavenly');
+  $('body').append($el);
+  window.setTimeout(function(){
+    $el.addClass('earthly');
+  }, 200);
+}
+
+window.setInterval(makeMeteors, 2000);
