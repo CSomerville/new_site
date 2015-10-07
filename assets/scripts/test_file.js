75635710ee3eb4a17c64c07cf58d1de60fd0d32d
Colby Somerville <colby.somerville@gmail.com>
2015-10-07 17:55:25 -0400

    probs

--- assets/scripts/test_file.js
+++ assets/scripts/test_file.js
@@ -1,14 +1,35 @@
 var allSkills = ['git', 'nodejs', 'postgresql', 'backbone', 'less', 
-                'sass', 'gulp', 'angular', 'ruby', 'rails', 'python']
+                'sass', 'gulp', 'angular', 'ruby', 'rails', 'python',
+                'react']
 
 function makeMeteors(){
+
+  if (Math.floor(Math.random() * 4) < 3) return;
+
   var skill = allSkills[Math.floor(Math.random () * allSkills.length)];
   var str = '<span class="devicons devicons-' + skill + '">';
-  var $el = $(str).addClass('heavenly');
-  $('body').append($el);
+  var pos = Math.floor(Math.random() * $(window).width());
+
+  var $el = $(str).addClass('heavenly').css('left', pos + 'px');
+  $('.backdrop-wrapper').prepend($el);
   window.setTimeout(function(){
-    $el.addClass('earthly');
+    $el.addClass('earthly').css('left', (pos - 200) + 'px');
   }, 200);
 }
 
-window.setInterval(makeMeteors, 2000);
+function cleanMeteors() {
+  $('.devicons').each(function(){
+    if (parseInt($(this).css('top')) === 1000) {
+      $(this).remove();
+    }
+  });
+};
+
+window.setInterval(function(){
+  makeMeteors();
+  cleanMeteors();
+}, 500);
+
+$('window').on('wheel', function(){
+  console.log('wtf')
+});
